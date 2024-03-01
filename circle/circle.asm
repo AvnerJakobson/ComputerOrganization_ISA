019d1ff69a84a7de5b810ee41b79c16a16ebd76e
Michael Cohen <michc200@gmail.com>
2024-03-01 17:20:07 +0200

    reworked circle, close to finishing

--- circle/circle.asm
+++ circle/circle.asm
@@ -1,29 +1,32 @@
  main:
-	lw $a0, $imm1, $zero, $zero, 0x100, 0 		# load R
-	add $t0, $zero, $zero, $zero, 0, 0 			# x = 0
-	add $t1, $a0, $zero, $zero, 0, 0 			# y = R
-	add $t2, $a0, $imm1, $zero, -1, 0 			# p = 1 - R
-	add $s0, $zero, $imm1, $zero, 128, 0 		# x_center = 128
-	add $s1, $zero, $imm1, $zero, 256, 0 		# y_center = 256
-	mac $s1, $s1, $s1, $imm1, 128, 0 			# y_center = 256*256 + 128, should be the center pixel
+	lw $a0, $imm1, $zero, $zero, 0x100, 0 			# load R
+	mac $a0, $a0, $a0, $zero, 0, 0 					# $a0 = R^2
+	out $zero, $zero, $imm1, $imm2, 21, 255 		# monitor data(IO21) = 255
+	add $t0, $zero, $zero, $zero, 0, 0 				# x_counter = 0
+	add $t1, $zero, $zero, $zero, 0, 0 				# y_counter = 0
 
-Loop:
-	bgt $zero, $t0, $t1, $imm1, END, 0 			# if x > y, END	
-	beq $zero, $zero, $zero, $imm1, plot, 0 	# plot
-	add $t0, $t0, $imm1, $zero, 1, 0 			# x++
-	blt, $zero, $t2, $zero, $imm1, inc_p, 0 	# if p < 0
-	add $t1, $t1, $imm1, $zero, -1, 0 			# y -= 1
-	sub $s2, $t0, $t1, $zero, 0, 0 				# $s2 = (x-y)
-	mac $s2, $s2, $imm1, $imm2, 2, 1 			# $s2 = 2(x-y) + 1
-	add $t2, $t2, $s2, $zero, 0, 0 				# p += 2(x-y) + 1
+
+outer_loop:
+	beq $zero, $zero, $zero, $imm2, 0, inner_loop 	# jump to inner loop
+	add $t1, $t1, $imm1, $zero, 1, 0 				# y++
+	add $t0, $zero, $zero, $zero, 0, 0 				# x_counter = 0
+	beq $zero, $zero, $zero, $imm2, 0, outer_loop 	# jump to outer_loop
 	
-inc_p:
-	add $s2, $t0, $t0, $imm1, 1, 0 				# $s2 = 2x + 1
-	add $t2, $t2, $s2, $zero, 0, 0				# p += 2x + 1
-	beq $zero, $zero, $zero, $imm1, Loop, 0 	# jump to loop
+inner_loop:
+	sub $t2, $t1, $imm1, $zero, 128, 0 				# y - y_center
+	mac $t2, $t2, $t2, $zero, 0, 0 					# $t2 = (y - y_center)^2
+	sub $s0, $t0, $imm1, $zero, 128, 0 				# $s0 = x - x-center
+	mac $s0, $s0, $s0, $zero, 0, 0 					# $s0 = (x - x-center)^2
+	add $t2, $t2, $s0, $zero, 0, 0 					# $t2 = (y - y_center)^2 + (x - x-center)^2
+	ble $zero, $t2, $a0, $imm1, plot, 0 			# if x-center^2 + y-center^2 <= R^2: plot
+	add $t0, $t0, $imm1, $zero, 1, 0 				# x++
+	# if x_counter == 255 and y_counter == 255 END
+	beq $zero, $t0, $imm1, outer_loop, 255, 0 		# else if x_counter == 255 outer loop
+	beq $zero, $zero, $zero, inner_loop, 0, 0 		# else return to inner_loop
 	
 plot:
 	# actual plot
-	beq $zero, $zero, $zero, $imm1, Loop, 0 	# jump to loop
+	beq $zero, $zero, $zero, inner_loop, 0, 0 		# return to inner loop
 	
 END:
+	
