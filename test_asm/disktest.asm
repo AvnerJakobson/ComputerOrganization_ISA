bd3e436efcf98ea37b3773a56a07f414600afd06
Michael Cohen <michc200@gmail.com>
2024-03-09 20:02:05 +0200

    debug disktest

--- test_asm/disktest.asm
+++ test_asm/disktest.asm
@@ -9,7 +9,7 @@ loop:
 	jal $ra, $zero, $zero, $imm1, wait_for_disk, 0 		# wait_for_disk to be free
 	out $zero, $imm1, $zero, $imm2, 14, 1				# diskcmd = 1, read
 	in $t0, $imm1, $zero, $zero, 15, 0 					# $t0 = disk sector
-	add $t0, $t0, $imm1, $zero, 1, 0 					# t0 ++
+	add $t0, $t0, $imm1, $zero, 1, 0 					# $t0 ++
 	out $zero, $imm1, $zero, $t0, 15, 0 				# disksector = $t0
 	jal $ra, $zero, $zero, $imm1, wait_for_disk, 0 		# wait_for_disk to be free
 	out $zero, $imm1, $zero, $imm2, 14, 2 				# diskcmd = 2, write to disksector + 1
@@ -17,6 +17,7 @@ loop:
 	add $t0, $t0, $imm1, $zero, -2, 0 					# t0 -= 2 
 	blt $zero, $t0, $imm1, $imm2, 0, END				# if $t0 < 0, jump to END
 	out $zero, $imm1, $zero, $t0, 15, 0 				# disksector = $t0
+	beq $zero, $zero, $zero, $imm1, loop, 0 			# return to loop
 
 wait_for_disk:
 	in $t1, $imm1, $zero, $zero, 17, 0 					# $t1 = diskstatus
