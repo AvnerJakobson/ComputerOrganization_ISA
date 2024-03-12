5d0131e6205c972dd6b6039900b45d2ce06b5e9b
Tomer Salton <tomersalton@mail.tau.ac.il>
2024-03-12 10:54:36 +0200

    build for sim and test_cases for asm

---
+++ test_asm_syn_change/circle.asm
@@ -0,0 +1,53 @@
+ .word 0x100 100
+        
+ main:
+ 	add $sp, $zero, $imm2, $zero, 0, 2000			# set stack at 2000
+	add $sp, $sp, $imm2, $zero, 0, -4				# adjust stack for 4 items
+	sw $zero, $sp, $imm2, $s0, 0, 3					# save $s0
+	sw $zero, $sp, $imm2, $s1, 0, 2					# save $s1
+	sw $zero, $sp, $imm2, $s2, 0, 1					# save $s2
+	sw $zero, $sp, $imm2, $a0, 0, 0 				# save $a0
+	lw $a0, $imm1,$zero, $zero, 0X100, 0 			# load R
+	mac $a0, $a0, $a0, $zero, 0, 0 					# $a0 = R^2
+	out $zero, $zero, $imm1, $imm2, 21, 255 		# monitor data(IO21) = 255
+	add $t0, $zero, $zero, $zero, 0, 0 				# x_counter = 0
+	add $t1, $zero, $zero, $zero, 0, 0 				# y_counter = 0
+	 #  
+outer_loop:
+	jal $ra, $zero, $zero, $imm2, 0, inner_loop 	# jump to inner loop
+	add $t1, $t1, $imm1, $zero, 1, 0 				# y++
+	#.word 0x100 100
+	add $t0, $zero, $zero, $zero, 0, 0 				# x_counter = 0
+	beq $zero, $zero, $zero, $imm2, 0, outer_loop 	# jump to outer_loop
+	
+
+
+inner_loop:
+	sub $t2, $t1, $imm1, $zero, 128, 0 				# $t2 = y - y_center
+	mac $t2, $t2, $t2, $zero, 0, 0 					# $t2 = (y - y_center)^2
+	sub $s0, $t0, $imm1, $zero, 128, 0 				# $s0 = x - x_center
+	mac $s0, $s0, $s0, $zero, 0, 0 					# $s0 = (x - x-center)^2
+	add $t2, $t2, $s0, $zero, 0, 0 					# $t2 = (y - y_center)^2 + (x - x-center)^2
+	ble $zero, $t2, $a0, $imm1, plot, 0 			# if x-center^2 + y-center^2 <= R^2: plot
+after_plot: 
+	add $t0, $t0, $imm1, $zero, 1, 0 				# x++
+	
+	add $s1, $t0, $t1, $zero, 0, 0 					# $s1 = x_counter + y_counter
+	beq $zero, $s1, $imm1, $imm2, 510, END 			# if x_counter == 255 and y_counter == 255 jump to END
+	beq $zero, $t0, $imm1, $ra, 255, 0 				# else if x_counter == 255 return to the line y++ in outer_loop
+	beq $zero, $zero, $zero, $imm1, inner_loop, 0 	# else return to inner_loop
+	
+plot:
+	mac $s2, $t1, $imm1, $t0, 256, 0 				# $s2 = 256*y_counter + x_counter (pixel address)
+	out $zero, $imm1, $zero, $s2, 20, 0 			# give pixel address to monitor address register(20)
+	out $zero, $imm1, $zero, $imm2, 22, 1 			# set monitorcmd register(22) to 1
+	out $zero, $imm1, $zero, $imm2, 22, 0 			# set monitorcmd register(22) to 0
+	beq $zero, $zero, $zero, $imm1, after_plot, 0 	# return to after_plot
+	
+END:
+	lw $s0, $sp, $imm2, $zero, 0, 0					# restore $a0
+	lw $s2, $sp, $imm2,$zero,0,1					# restore $s2
+	lw $s1, $sp, $imm2, $zero, 0, 2					# restore $s1
+	lw $s0, $sp,$imm2, $zero, 0, 3					# restore $s0
+	add $sp, $sp, $imm2, $zero, 0, 4				# adjust stack pointer
+	halt $zero, $zero, $zero, $zero, 0, 0			# halt
---
+++ test_asm_syn_change/disktest.asm
@@ -0,0 +1,52 @@
+
+
+
+
+main:
+	add $sp, $zero, $imm2, $zero, 0, 2000				# set stack at 2000
+	out $zero, $imm1, $zero, $imm2, 6, ISR				# irqhandler PC = ISR
+	out $zero, $imm1, $zero, $imm2, 1, 1 				# set irq1enable to 1
+	out $zero, $imm1, $zero, $imm2, 15, 7 				# disksector = 7
+	out $zero, $imm1, $zero, $imm2, 16, 500 			# diskbuffer = 500
+
+loop:
+	jal $ra, $zero, $zero, $imm1, wait_for_disk, 0 		# wait_for_disk to be free
+	out $zero, $imm1, $zero, $imm2, 14, 1				# diskcmd = 1, read
+	in $t0, $imm1, $zero, $zero, 15, 0 					# $t0 = disk sector
+	add $t0, $t0, $imm1, $zero, 1, 0 					# $t0 ++
+	out $zero, $imm1, $zero, $t0, 15, 0 				# disksector = $t0
+
+###dfvdfvdfv
+	jal $ra, $zero, $zero, $imm1, wait_for_disk, 0 		# wait_for_disk to be free
+	out $zero, $imm1, $zero, $imm2, 14, 2 				# diskcmd = 2, write to disksector + 1
+	jal $ra, $zero, $zero, $imm1, wait_for_disk, 0 		# wait_for_disk to be free
+	add $t0, $t0, $imm1, $zero, -2, 0 					# t0 -= 2 
+	blt $zero, $t0, $imm1, $imm2, 0, END				# if $t0 < 0, jump to END
+	out $zero, $imm1, $zero, $t0, 15, 0 				# disksector = $t0
+	beq $zero, $zero, $zero, $imm1, loop, 0 			# return to loop
+
+wait_for_disk:
+	in $t1, $imm1, $zero, $zero, 17, 0 					# $t1 = diskstatus
+	beq $zero, $t1, $imm1, $ra, 0, 0 					# if diskstatus == 0, return to the loop thru $ra
+	beq $zero, $zero, $zero, $imm1, wait_for_disk, 0 	# jump to wait_for_disk
+
+ISR:
+	out $zero, $imm1, $zero, $imm2, 17, 0 				# set diskstatus to 0
+	out $zero, $imm1, $zero, $imm2, 4, 0 				# set irq1status(IO4) to 0
+	reti $zero, $zero, $zero, $zero, 0, 0				# return from interrupt
+	
+END:
+	halt $zero, $zero, $zero, $zero, 0, 0				# halt
+
+
+
+
+
+
+
+
+
+
+
+
+
